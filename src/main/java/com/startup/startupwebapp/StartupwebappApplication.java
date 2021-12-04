579353655e6c72271275a513615ad49f497d8b2c
csapnu-07 <83846883+csapnu-07@users.noreply.github.com>
2021-12-04 13:27:07 +0800

    startupwebapp github repo
    1st commit

---
+++ src/main/java/com/startup/startupwebapp/StartupwebappApplication.java
@@ -0,0 +1,13 @@
+package com.startup.startupwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startup/startupwebapp/StartupwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.startupwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
